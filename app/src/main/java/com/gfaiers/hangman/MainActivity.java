6aba2f3af6458e42968323d4eb0005abe5ce4e00
Geoff Faiers <geoff@gfaiers.com>
2016-07-01 22:22:10 +0100

    Updated to 24.0.0
    Trying to resolve error with BaseGameUtils

--- app/src/main/java/com/gfaiers/hangman/MainActivity.java
+++ app/src/main/java/com/gfaiers/hangman/MainActivity.java
@@ -55,14 +55,6 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Create the Google Api Client with access to the Play Games services
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(Games.API).addScope(Games.SCOPE_GAMES)
-                // add other APIs and scopes here as needed
-                .build();
-
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
@@ -73,6 +65,14 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                 .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
                 .build();
         mAdView.loadAd(adRequest);
+
+        // Create the Google Api Client with access to the Play Games services
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Games.API).addScope(Games.SCOPE_GAMES)
+                // add other APIs and scopes here as needed
+                .build();
 
         // Initialize the components
         Button buttonNewGame = (Button) findViewById(R.id.buttonNewGame);
